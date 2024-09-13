b2002fea21036891fbe070e5411ca3410f676958
mania605 <kim.minsun83@gmail.com>
2024-09-13 11:11:13 +0900

    생성자함수 호출시 객체를 인수로 전달해 인스턴스에 다른값 적용

--- script.js
+++ script.js
@@ -22,9 +22,9 @@
 
 //객체 리터럴 방식
 const student1 = {
-  name: 'andy';
+  name: 'andy',
   age: 20,
-  isFemale: false,
+  isFemale: false
 };
 
 console.log(student1.name);
@@ -51,15 +51,18 @@ student2.inform();
 //객체리터럴 안쪽에서의 this: widnow객체 (전역객체)
 
 //생성자를 통한 인스턴스를 복사하는 법
-function Student() {
-  this.name = 'Emily';
-  this.age = 20;
-  this.isFemale = true;
+function Student(props) {
+  this.name = props.name;
+  this.age = props.age || 20; //대입되는 값 뒤에 || 연산자를 입력하면 해당 값이 undeined대신 대체값을 설정
+  this.isFemale = props.isFemale;
 }
-
 //아래와 같이 new 연산자를 이용해서 Student 생성자 함수 호출하면
 //인스턴스라는 특별한 복사본 객체를 생성가능
 //인스턴스 객체는 같은 생성자함수를 통해서 생성된 모든 인스턴스들이 공유할 수 있는 prototype이라는 공간을 공유
 //prototype: 해당 복사본 객체들이 공통적으로 활용해야 되는 함수를 등록 (메서드)
-const studentCopy1 = new Student();
+const studentCopy1 = new Student({ name: 'Emily', isFemale: true });
+const studentCopy2 = new Student({ name: 'David', isFemale: false });
+const studentCopy3 = new Student({ name: 'Michael', isFemale: false, age: 19 });
 console.log(studentCopy1);
+console.log(studentCopy2);
+console.log(studentCopy3);
